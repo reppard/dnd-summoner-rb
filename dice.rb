aa31578dee5d74bd317650c2d00c55348a7919ab
Reppard <reppardwalker@gmail.com>
2018-11-05 18:28:07 -0500

    dice

---
+++ dice.rb
@@ -0,0 +1,9 @@
+require_relative 'lib/scoreset'
+require_relative 'lib/race_picker'
+require_relative 'lib/class_picker'
+require_relative 'lib/character'
+
+puts "Welcome to the D&D Character Creator"
+char  = Character.new
+
+puts char
